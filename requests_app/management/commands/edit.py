5faeb3e97725328ba9607b1cb54a57d887d98968
corbin-chris <chris.corbin2@nhs.net>
2023-07-04 15:27:52 +0100

    Get kwargs from command. Check expected arguments are present.

--- requests_app/management/commands/edit.py
+++ requests_app/management/commands/edit.py
@@ -16,23 +16,21 @@ class Command(BaseCommand):
         "Commands to let the user edit the database from the command line."
     )
 
+    #def _validate_panel_exists(self, panel) -> bool:
+        #TODO: need to check the panel exists
 
-    def _validate_directory(self, path) -> bool:
-        return os.path.exists(path)
-    
 
     def add_arguments(self, parser) -> None:
         """Define the source of the data to import."""
 
-        # python manage.py seed --debug panelapp <panel> <version>
-        # python manage.py edit panel_clinical_ind clinical_indication <panel> <add_or_remove> <r_code>
+        # python manage.py edit panel_clinical_ind <panel> <add_or_remove> <r_code>
         subparsers = parser.add_subparsers(dest="command")
 
-        # Parser for panelapp command e.g. panelapp all or panelapp 1234
+        # Parser for command
         panel_clinical_ind = subparsers.add_parser("panel_clinical_ind", \
                                                    help="add or remove panel information for a particular panel")
         panel_clinical_ind.add_argument(
-            "panel",
+            "panel_id",
             type=str,
             help="PanelApp panel, by ID",
         )
@@ -51,3 +49,33 @@ class Command(BaseCommand):
             help="Clinical indication, by R code",
         )
 
+    def handle(self, *args, **kwargs) -> None:
+        """Coordinates functions to import and parse data from
+        specified source, then calls inserter to insert cleaned data
+        into the database."""
+        print(kwargs)
+
+        test_mode: bool = kwargs.get("debug", False)
+        command: str = kwargs.get("command")
+
+        assert command, "Command error"
+
+        # TODO: validate panel_id exists
+        # TODO: validate r_code exists - prompt for it to be added to the database otherwise
+
+        # python manage.py edit panel_clinical_ind <panel_id> <add_or_remove> <clinical_indication>
+        if command == "panel_clinical_ind":
+            panel_id: str = kwargs.get("panel_id")
+            add_or_remove: str = kwargs.get("add_or_remove")
+            r_code: str = kwargs.get("r_code")
+
+            if not panel_id:
+                raise ValueError("Please specify panel ID")
+            
+            if not add_or_remove:
+                raise ValueError("Please specify whether you want to add or remove the clinical \
+                                indication for this panel")
+            
+            if not r_code:
+                raise ValueError("Please specify R code")
+        
