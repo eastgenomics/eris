b1ffad15f31e84f4ba6ca86182073da510bdd2ed
corbin-chris <chris.corbin2@nhs.net>
2023-08-04 18:49:46 +0100

    TestFlagActiveLinksForPanel - now tests something useful!

--- tests/tests_requests_app/test_insert_panel.py
+++ tests/tests_requests_app/test_insert_panel.py
@@ -45,8 +45,10 @@ class TestFlagActiveLinksForPanel(TestCase):
 
     def test_flag_ci_panel_instances_controller(self):
         """
-        Create a new version of a panel which already has a link to a CI
-        Check that the CI"""
+        Create a new version of a panel. A panel with the same external ID is
+        already linked to a CI in the database.
+        Check that the CI in the database is changed to 'needs review' and has history logged
+        """
         new_panel, created = Panel.objects.get_or_create(
             external_id="one_test_panel_id",
             panel_name="one_test_panel_name",
@@ -60,11 +62,22 @@ class TestFlagActiveLinksForPanel(TestCase):
             }
         )
 
-        ci_panel_instances = flag_active_links_for_panel(new_panel.pk, 
-                                                         new_panel.panel_name,
+        ci_panel_instances = flag_active_links_for_panel(new_panel.external_id, 
                                                          "test_user")
 
-        assert len(ci_panel_instances) == 2
+        # Expect a QuerySet of 1 ClinicalIndicationPanel result, because this function only flags 
+        # existing CI-Panel links, 
+        # and the new panel hasn't had one made yet
+        assert len(ci_panel_instances) == 1
+        assert type(ci_panel_instances) == QuerySet
+
+        # Check that the ClinicalIndicationPanel has been set to needs_review = True
+        ci_first = ci_panel_instances.all()[:1].get()
+        assert ci_first.config_source == "unit_test"
+        assert ci_first.needs_review == True
+
+        # TODO: Check for a history record for ClinicalIndicationPanel
+
 
 
 class test_make_provisional_ci_panel_link_with_panel(TestCase):
@@ -184,11 +197,11 @@ class test_make_provisional_ci_panel_link_with_panel(TestCase):
             }
         )
 
-        fetched_panel = Panel.objects.filter(external_id="one_test_panel_id")
-        assert len(fetched_panel) == 2
+        # fetched_panel = Panel.objects.filter(external_id="one_test_panel_id")
+        # assert len(fetched_panel) == 2
 
-        user = "test_user"
-        panel_or_ci = "panel"
+        # user = "test_user"
+        # panel_or_ci = "panel"
 
 
         # # run make_provisional_ci_panel_link on the contents of the isolated database
