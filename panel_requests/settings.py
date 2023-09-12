f9012cb5509239cb718ba7e876f2c233bac36691
JasonLing95 <jason_ling95@hotmail.com>
2023-09-12 11:19:34 +0100

    upload genepanel to dnanexus

--- panel_requests/settings.py
+++ panel_requests/settings.py
@@ -147,3 +147,5 @@ HGNC_IDS_TO_OMIT = (
 PANELAPP_API_URL = os.environ.get(
     "PANELAPP_API_URL", "https://panelapp.genomicsengland.co.uk/api/v1/panels/"
 )
+
+DNANEXUS_TOKEN = os.environ.get("DNANEXUS_TOKEN", None)  # fetch dnanexus token
