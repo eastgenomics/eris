6b627b739d011658a37d6e29088987f5376d9140
corbin-chris <chris.corbin2@nhs.net>
2023-09-20 16:22:11 +0100

    Test added for MANE sanity-checker

--- tests/tests_requests_app/test_management/test_commands/test_parse_transcript/test_prepare_mane_file.py
+++ tests/tests_requests_app/test_management/test_commands/test_parse_transcript/test_prepare_mane_file.py
@@ -13,3 +13,25 @@ class TestSanityCheckManeFile(TestCase):
     def test_passing_case(self):
         test_mane = pd.DataFrame({"Gene": [], "MANE TYPE": [], "RefSeq StableID GRCh38 / GRCh37": []})
         assert not _sanity_check_mane_file(test_mane)
+
+    def test_missing_gene(self):
+        test_mane = pd.DataFrame({"MANE TYPE": [], "RefSeq StableID GRCh38 / GRCh37": []})
+        with self.assertRaisesRegex(AssertionError, "Missing Gene column. Check MANE file"): 
+            _sanity_check_mane_file(test_mane)
+
+    def test_missing_mane_type(self):
+        test_mane = pd.DataFrame({"Gene": [], "RefSeq StableID GRCh38 / GRCh37": []})
+        with self.assertRaisesRegex(AssertionError, "Missing MANE TYPE column. Check MANE file"): 
+                    _sanity_check_mane_file(test_mane)
+    
+    def test_missing_refseq(self):
+        test_mane = pd.DataFrame({"Gene": [], "MANE TYPE": []})
+        with self.assertRaisesRegex(AssertionError, "Missing RefSeq column. Check MANE file"): 
+            _sanity_check_mane_file(test_mane)
+
+    def test_missing_several(self):
+        test_mane = pd.DataFrame({"Gene": []})
+        with self.assertRaisesRegex(AssertionError, "Missing MANE TYPE column. Check MANE file; " + 
+                                    "Missing RefSeq column. Check MANE file"): 
+            _sanity_check_mane_file(test_mane)
+
