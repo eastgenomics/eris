4813146427b4833463666574d929614bb686b648
corbin-chris <chris.corbin2@nhs.net>
2023-10-03 15:55:52 +0100

    Checked more db tables for _add_clinical_gene_and_transcript_to_db

--- tests/tests_requests_app/test_management/test_commands/test_parse_transcript/test_add_clinical_gene_and_transcript_to_db.py
+++ tests/tests_requests_app/test_management/test_commands/test_parse_transcript/test_add_clinical_gene_and_transcript_to_db.py
@@ -2,7 +2,8 @@ from django.test import TestCase
 
 
 from requests_app.models import \
-    Gene, Transcript, TranscriptSource
+    Gene, Transcript, TranscriptSource, TranscriptRelease,\
+    TranscriptReleaseLink
 
 
 from requests_app.management.commands._parse_transcript import \
@@ -22,7 +23,9 @@ class TestAddClinicalGeneTranscript_FromScratch(TestCase):
     
     def test_add_new_gene_new_transcript_single(self):
         """
-        Straightforward use case, one transcript per gene
+        Straightforward use case: there is one transcript per gene.
+        We expect the transcript, gene, release, source of data, and link between tx
+        and gene to be added to the db.
         """
         err = []
 
@@ -42,6 +45,8 @@ class TestAddClinicalGeneTranscript_FromScratch(TestCase):
         new_genes = Gene.objects.all()
         new_sources = TranscriptSource.objects.all()
         new_transcripts = Transcript.objects.all()
+        new_links = TranscriptReleaseLink.objects.all()
+        new_release = TranscriptRelease.objects.all()
 
         # Check that the expected genes were added to the database
         err += len_check_wrapper(new_genes, "gene", 1)
@@ -54,8 +59,20 @@ class TestAddClinicalGeneTranscript_FromScratch(TestCase):
 
         # Check that the transcripts were added
         err += len_check_wrapper(new_transcripts, "transcript", 1)
-        err += value_check_wrapper(new_transcripts[0].transcript, "transcript name", "NM00045.6")
+        err += value_check_wrapper(new_transcripts[0].transcript, "transcript name",
+                                   "NM00045.6")
         err += value_check_wrapper(new_transcripts[0].reference_genome, "ref", "37")
+
+        # Check that there's a release
+        err += len_check_wrapper(new_release, "release", 1)
+        err += value_check_wrapper(new_release[0].external_release_version, "release",
+                                   "release_2")
+
+        # Check that the transcript is linked to the release
+        err += len_check_wrapper(new_links, "links", 1)
+        err += value_check_wrapper(new_links[0].match_version, "link match version", True)
+        err += value_check_wrapper(new_links[0].release, "link release", new_release[0])
+        err += value_check_wrapper(new_links[0].transcript, "link tx", new_transcripts[0])
 
         errors = "; ".join(err)
         assert not errors, errors
@@ -66,9 +83,8 @@ class TestAddGeneTranscript_AlreadyExists(TestCase):
     Cases where transcripts are being added to a database which
     already contains a gene and transcript.
 
-    The second transcript will be added. Most of the time this is
-    NOT desired behaviour, however, it may be in some circumstances
-    (e.g. 37 having a different transcript from 38)
+    The second transcript will be added - this can happen if e.g. the
+    reference genome for one is 37, and for the other, it is 38.
     """
 
     def setUp(self) -> None:
@@ -113,6 +129,8 @@ class TestAddGeneTranscript_AlreadyExists(TestCase):
                                                 file_info)
 
         all_transcripts = Transcript.objects.all()
+        new_links = TranscriptReleaseLink.objects.all()
+        new_release = TranscriptRelease.objects.all()
 
         # expect old and new copy for the same transcript
         err += len_check_wrapper(all_transcripts, "transcript", 2)
@@ -124,6 +142,18 @@ class TestAddGeneTranscript_AlreadyExists(TestCase):
         new_tx = all_transcripts[1]
         err += value_check_wrapper(new_tx.transcript, "transcript name", "NM00045.6")
         err += value_check_wrapper(new_tx.reference_genome, "ref", "38")
+
+        # Check that there's a release in the database
+        err += len_check_wrapper(new_release, "release", 1)
+        err += value_check_wrapper(new_release[0].external_release_version, "release",
+                                   "release_2")
+
+        # Check that there's only one link between transcript (our new one, as we didn't
+        # make a link at set-up) and the release version
+        err += len_check_wrapper(new_links, "links", 1)
+        err += value_check_wrapper(new_links[0].match_version, "link match version", False)
+        err += value_check_wrapper(new_links[0].release, "link release", new_release[0])
+        err += value_check_wrapper(new_links[0].transcript, "link tx", new_tx)
 
         errors = "; ".join(err)
         assert not errors, errors
