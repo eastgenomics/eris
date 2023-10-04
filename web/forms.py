a63ed051c9d354a3fd7580294c0b4ca20126fe6d
JasonLing95 <jason_ling95@hotmail.com>
2023-10-04 14:28:21 +0100

    more filter on gene submit

--- web/forms.py
+++ web/forms.py
@@ -57,8 +57,14 @@ class GeneForm(forms.Form):
     def clean_hgnc_id(self):
         hgnc_id: str = self.cleaned_data["hgnc_id"].upper()
 
+        if not hgnc_id:
+            self.add_error("hgnc_id", "HGNC ID cannot be empty!")
+
         if not hgnc_id.startswith("HGNC:"):
-            self.add_error("hgnc_id", "HGNC ID must start with HGNC:")
+            self.add_error("hgnc_id", "HGNC ID must start with 'HGNC:'")
+
+        if not hgnc_id[5:].isdigit():
+            self.add_error("hgnc_id", "HGNC ID must be followed by numbers!")
 
         try:
             Gene.objects.get(hgnc_id=hgnc_id)
@@ -69,3 +75,17 @@ class GeneForm(forms.Form):
             pass
 
         return hgnc_id
+
+    def clean_gene_symbol(self):
+        gene_symbol = self.cleaned_data["gene_symbol"].strip().upper()
+
+        try:
+            Gene.objects.get(gene_symbol=gene_symbol)
+
+            self.add_error("gene_symbol", "Gene with this gene symbol already exists!")
+
+        except:
+            # no gene with this gene_symbol exists, that's good
+            pass
+
+        return gene_symbol
