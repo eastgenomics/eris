7315650a49c215b70c293a90ba30f23231c16591
corbin-chris <chris.corbin2@nhs.net>
2023-10-18 10:14:55 +0100

    Finished tests for superpanel class

--- tests/tests_requests_app/test_management/test_commands/test_panelapp/test_superpanelclass.py
+++ tests/tests_requests_app/test_management/test_commands/test_panelapp/test_superpanelclass.py
@@ -121,23 +121,122 @@ class TestSuperPanelClass(TestCase):
             "15q11q13 recurrent (PWS/AS) region (BP1-BP3, Class 1) Loss"
         assert first_panels_regions[0]["type_of_variants"] =="cnv_loss"
 
-    def test_multiple_genes_multiple_panels(self):
+    def test_gene_and_region_from_same_panel(self):
         """
         A situation where some panels and genes are from the same panel.
-        EXPECT: panels and genes from the same panel should append correctly 
+        EXPECT: 1 gene and 1 region from the same panel should append correctly 
         to the same, single PanelClass object.
         """
-        #TODO: write
-        pass
+        json_response = mocked_requests_get_superpanel(
+            "testing_files/panelapp_api_mocks/superpanel_api_example_gene_region_same_panel.json"
+        )
+        superpanel = SuperPanelClass(**json_response)
+        component_panels = superpanel.create_component_panels(superpanel.genes,
+                                                              superpanel.regions)
+        
+        # our test data contains only 2 regions, and these are both for the same panel
+        # the genes are removed from the test data so shouldn't affect anything
+        assert len(component_panels) == 1
+
+        assert len(component_panels[0].genes) == 1
+        assert len(component_panels[0].regions) == 1
+
+        first_panel = component_panels[0]
+        assert first_panel.id == 79
+        assert first_panel.name == "Paediatric motor neuronopathies"
+        assert first_panel.version == "3.4"
+        assert first_panel.panel_source == "PanelApp"
+
+        first_panel_genes = first_panel.genes
+        assert len(first_panel_genes) == 1
+        assert first_panel_genes[0]["gene_data"]["hgnc_id"] == "HGNC:735"
+        assert first_panel_genes[0]["gene_data"]["gene_name"] == \
+            "N-acylsphingosine amidohydrolase 1"
+
+        first_panels_regions = component_panels[0].regions
+        assert first_panels_regions[0]["entity_name"] == "ISCA-37404-Loss"
+        assert first_panels_regions[0]["verbose_name"] == \
+            "15q11q13 recurrent (PWS/AS) region (BP1-BP3, Class 1) Loss"
+        assert first_panels_regions[0]["type_of_variants"] == "cnv_loss"
 
     def test_gene_occurs_in_more_than_one_panel(self):
-        #TODO: write
-        pass
+        """
+        A situation where a gene appears in TWO subpanels
+        EXPECT: gene to be attributed to both panels, with info such as MOI 
+        separated out correctly
+        """
+        json_response = mocked_requests_get_superpanel(
+            "testing_files/panelapp_api_mocks/superpanel_api_example_one_gene_two_panels.json"
+        )
+        superpanel = SuperPanelClass(**json_response)
+        component_panels = superpanel.create_component_panels(superpanel.genes,
+                                                              superpanel.regions)
+        
+        assert len(component_panels) == 2
+
+        first_panel = component_panels[0]
+        assert first_panel.id == 225
+        assert first_panel.name == "Congenital myopathy"
+        assert first_panel.version == "4.31"
+        assert first_panel.panel_source == "PanelApp"
+
+        first_panel_genes = first_panel.genes
+        assert len(first_panel_genes) == 1
+        assert first_panel_genes[0]["gene_data"]["hgnc_id"] == "HGNC:3756"
+        assert first_panel_genes[0]["gene_data"]["gene_name"] == \
+            "filamin C"
+
+        second_panel = component_panels[1]
+        assert second_panel.id == 185
+        assert second_panel.name == \
+            "Limb girdle muscular dystrophies, myofibrillar myopathies and distal myopathies"
+        assert second_panel.version == "4.22"
+        assert second_panel.panel_source == "PanelApp"
+
+        second_panel_genes = second_panel.genes
+        assert len(second_panel_genes) == 1
+        assert second_panel_genes[0]["gene_data"]["hgnc_id"] == "HGNC:3756"
+        assert second_panel_genes[0]["gene_data"]["gene_name"] == \
+            "filamin C"
+
 
     def test_region_occurs_in_more_than_one_panel(self):
-        #TODO: write
-        pass
+        """
+        A situation where a region appears in TWO subpanels
+        EXPECT: region to be attributed to both panels, with info such as MOI 
+        separated out correctly
+        NOTE that the example regions file here was made by duplicating a region
+         from 'superpanel_api_example_one_gene_two_panels.json; and then
+        changing the panel in the JSON, while the other files were just made 
+        by deleting sections of 'real' API calls
+        """
+        json_response = mocked_requests_get_superpanel(
+            "testing_files/panelapp_api_mocks/superpanel_api_example_one_region_two_panels.json"
+        )
+        superpanel = SuperPanelClass(**json_response)
+        component_panels = superpanel.create_component_panels(superpanel.genes,
+                                                              superpanel.regions)
+        
+        assert len(component_panels) == 2
 
-    def test_region_and_gene_occurs_in_more_than_one_panel(self):
-        #TODO: write
-        pass
+        first_panel = component_panels[0]
+        assert first_panel.id == 79
+        assert first_panel.name == "Paediatric motor neuronopathies"
+        assert first_panel.version == "3.4"
+        assert first_panel.panel_source == "PanelApp"
+
+        first_panel_regions = first_panel.regions
+        assert len(first_panel_regions) == 1
+        assert first_panel_regions[0]["entity_name"] == "ISCA-37404-Loss"
+
+        second_panel = component_panels[1]
+        assert second_panel.id == 100
+        assert second_panel.name == \
+            "Other motor neuronopathies"
+        assert second_panel.version == "3.6"
+        assert second_panel.panel_source == "PanelApp"
+
+        second_panel_regions = second_panel.regions
+        assert len(second_panel_regions) == 1
+        assert second_panel_regions[0]["entity_name"] == "ISCA-37404-Loss"
+    
