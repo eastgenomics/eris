1ef30c2dfa2f9ebf2a681072d95171f63feb9ee6
corbin-chris <chris.corbin2@nhs.net>
2023-11-02 11:51:35 +0000

    Test refactor

--- tests/tests_requests_app/test_management/test_commands/test_parse_transcript/test_get_or_create_gene_from_db.py
+++ tests/tests_requests_app/test_management/test_commands/test_parse_transcript/test_get_or_create_gene_from_db.py
@@ -1,10 +1,8 @@
 from django.test import TestCase
 
-from unittest import mock
-import pandas as pd
-from django.db import transaction
-
 from requests_app.management.commands._parse_transcript import _add_new_genes_to_db
+from tests.tests_requests_app.test_management.test_commands.test_insert_ci.test_insert_test_directory_data \
+import len_check_wrapper
 from requests_app.models import Gene
 
 
@@ -18,16 +16,24 @@ class TestGetOrCreate_CreateNew(TestCase):
         pass
 
     def test_adding_identical_gene(self):
+        errors = []
+
         approved_symbols = {"HGNC:10257": "ROR2", "HGNC:TEST": "TEST_SYMBOL"}
         alias_symbols = {"HGNC:10257": None, "HGNC:TEST": "TEST, ALIAS"}
 
         _add_new_genes_to_db(approved_symbols, alias_symbols)
 
         post_run_genes = Gene.objects.all()
-        assert len(post_run_genes) == 2
+        errors += len_check_wrapper(post_run_genes, "number of post run genes", 2)
 
         poss_symbols = ["ROR2", "TEST_SYMBOL"]
         poss_aliases = [None, "TEST, ALIAS"]
         for i in range(0, 2, 1):
-            assert post_run_genes[i].gene_symbol in poss_symbols
-            assert post_run_genes[i].alias_symbols in poss_aliases
+            if not post_run_genes[i].gene_symbol in poss_symbols:
+                errors.append(f"Post run gene {post_run_genes[i].gene_symbol} not in {poss_symbols}")
+            if not post_run_genes[i].alias_symbols in poss_aliases:
+                errors.append(f"Post run gene {post_run_genes[i].alias_symbols} not in {poss_aliases}")
+
+        errors = ";".join(errors)
+        assert not errors, errors 
+        
