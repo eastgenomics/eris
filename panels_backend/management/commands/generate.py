7e250ec19d05a321810066fa4479dda44f805072
corbin-chris <chris.corbin2@nhs.net>
2024-01-23 15:27:51 +0000

    Change g2t clinical outputs to 'clinical_transcript'/'not_clinical_transcript' instead of 'true/false'.

--- panels_backend/management/commands/generate.py
+++ panels_backend/management/commands/generate.py
@@ -444,7 +444,7 @@ class Command(BaseCommand):
             transcript_data = {
                 "hgnc_id": transcript.gene.hgnc_id,
                 "transcript": transcript.transcript,
-                "clinical": clinical_status,
+                "clinical": ["clinical_transcript" if clinical_status else "not_clinical_transcript"],
             }
             results.append(transcript_data)
 
@@ -538,7 +538,7 @@ class Command(BaseCommand):
 
             try:
                 # if the genome is valid, run the controller function, _generate_g2t
-                genome = ReferenceGenome.objects.get(reference_genome=parsed_genome)
+                genome = ReferenceGenome.objects.get(name=parsed_genome)
             except ObjectDoesNotExist:
                 raise ObjectDoesNotExist(
                     "Aborting g2t: reference genome does not exist in the database"
