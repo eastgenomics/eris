652fa4008d2d41b54580a73c882ab94191ce7dd4
corbin-chris <chris.corbin2@nhs.net>
2024-01-26 15:54:13 +0000

    Make sure panels without PanelApp IDs display blank strings for the external_id field

--- panels_backend/management/commands/generate.py
+++ panels_backend/management/commands/generate.py
@@ -210,7 +210,8 @@ class Command(BaseCommand):
             for panel_dict in panel_list:
                 # for each panel associated with that clinical indication
                 panel_id: str = panel_dict["ci_panel__panel_id"]
-                panel_panelapp_id: str = panel_dict["ci_panel__panel__external_id"]
+                # get the PanelApp external ID if there is one. If it's none, make it a blank string
+                panelapp_id: str = (panel_dict["ci_panel__panel__external_id"] if panel_dict["ci_panel__panel__external_id"] else "")
                 ci_name: str = panel_dict["ci_panel__clinical_indication_id__name"]
                 for hgnc in panel_genes[panel_id]:
                     # for each gene associated with that panel
@@ -227,7 +228,7 @@ class Command(BaseCommand):
                         f"{r_code}_{ci_name}",
                         f"{panel_dict['ci_panel__panel__panel_name']}_{panel_version}",
                         hgnc,
-                        f"{panel_dict['ci_panel__panel__external_id']}"
+                        f"{panelapp_id}"
                     ]
                     results.append(line)
         results = sorted(results, key=lambda x: [x[0], x[1], x[2]])
