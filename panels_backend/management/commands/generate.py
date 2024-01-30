0adb29f9f9953e5f6e3f194e2d221db0735dfb45
corbin-chris <chris.corbin2@nhs.net>
2024-01-30 09:56:47 +0000

    PR changes, removed redundant TODO, reformats

--- panels_backend/management/commands/generate.py
+++ panels_backend/management/commands/generate.py
@@ -211,11 +211,7 @@ class Command(BaseCommand):
                 # for each panel associated with that clinical indication
                 panel_id: str = panel_dict["ci_panel__panel_id"]
                 # get the PanelApp external ID if there is one. If it's none, make it a blank string
-                panelapp_id: str = (
-                    panel_dict["ci_panel__panel__external_id"]
-                    if panel_dict["ci_panel__panel__external_id"]
-                    else ""
-                )
+                panelapp_id: panel_dict.get("ci_panel__panel__external_id", "")
                 ci_name: str = panel_dict["ci_panel__clinical_indication_id__name"]
                 for hgnc in panel_genes[panel_id]:
                     # for each gene associated with that panel
@@ -223,16 +219,14 @@ class Command(BaseCommand):
                         continue
 
                     # process the panel version
-                    panel_version: str = (
-                        normalize_version(panel_dict["ci_panel__panel__panel_version"])
-                        if panel_dict["ci_panel__panel__panel_version"]
-                        else None
+                    panel_version: str = normalize_version(
+                        panel_dict.get("ci_panel__panel__panel_version", None)
                     )
                     line = [
                         f"{r_code}_{ci_name}",
                         f"{panel_dict['ci_panel__panel__panel_name']}_{panel_version}",
                         hgnc,
-                        f"{panelapp_id}",
+                        panelapp_id,
                     ]
                     results.append(line)
         results = sorted(results, key=lambda x: [x[0], x[1], x[2]])
@@ -287,7 +281,7 @@ class Command(BaseCommand):
                             f"{r_code}_{ci_name}",
                             f"{panel_dict['ci_superpanel__superpanel__panel_name']}_{panel_version}",
                             hgnc,
-                            f"{panelapp_id}",
+                            panelapp_id,
                         ]
                     )
         results = sorted(results, key=lambda x: [x[0], x[1], x[2]])
@@ -453,13 +447,13 @@ class Command(BaseCommand):
             clinical_status = self.get_current_transcript_clinical_status_for_g2t(
                 transcript, latest_select, latest_plus_clinical, latest_hgmd
             )
-            # TODO: add a canonical/non_canonical column
+            displayable_clinical_status = (
+                "clinical_transcript" if clinical_status else "not_clinical_transcript"
+            )
             transcript_data = {
                 "hgnc_id": transcript.gene.hgnc_id,
                 "transcript": transcript.transcript,
-                "clinical": "clinical_transcript"
-                if clinical_status
-                else "not_clinical_transcript",
+                "clinical": displayable_clinical_status,
             }
             results.append(transcript_data)
 
