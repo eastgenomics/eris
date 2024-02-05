9be69ccbe209200d3ffbce5a49751f92f956394c
spaul91 <sophie.paul@addenbrookes.nhs.uk>
2024-02-05 11:42:48 +0000

    removed redundant test script

--- variant_db/management/commands/test.py
+++
@@ -1,5 +0,0 @@
-import variant_db.management.commands.workbook as workbook
-import variant_db.management.commands.insert as insert
-wb_dict = workbook.read_workbook("misc/sample_new_format_SP.csv")
-print(wb_dict[0])
-insert.insert_row(wb_dict[0])
