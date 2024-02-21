2847eb8450d8e8e893cebfa8bd3b19a41559e751
corbin-chris <chris.corbin2@nhs.net>
2024-02-21 10:09:33 +0000

    Some test set-up

--- tests/test_panels_backend/test_management/test_commands/test_generate/test_generate_genepanels_results.py
+++ tests/test_panels_backend/test_management/test_commands/test_generate/test_generate_genepanels_results.py
@@ -10,6 +10,8 @@ from panels_backend.models import (
     SuperPanel,
     Gene,
     ClinicalIndication,
+    ClinicalIndicationPanel,
+    ClinicalIndicationSuperPanel,
     CiPanelTdRelease,
     CiSuperpanelTdRelease
 )
@@ -36,15 +38,31 @@ class TestGenerateGenepanels(TestCase):
             config_source="config source",
             td_date="2022-01-01"
         )
-        self.td_release_old = TestDirectoryRelease.objects.create(
-            release="3",
-            td_source="old source",
+        self.td_release_new = TestDirectoryRelease.objects.create(
+            release="4",
+            td_source="new source",
             config_source="config source",
-            td_date="2022-01-01"
+            td_date="2023-01-01"
         )
 
-        # Muliple panels and superpanels on that TdRelease - each has
-        # links to Genes
+        # Clinical Indications
+        self.ci_1 = ClinicalIndication.objects.create(
+            r_code="R1",
+            name="Common condition 1",
+            test_method="wgs"
+        )
+        self.ci_2 = ClinicalIndication.objects.create(
+            r_code="R2",
+            name="Common condition 2",
+            test_method="wgs"
+        )
+        self.ci_3 = ClinicalIndication.objects.create(
+            r_code="R3",
+            name="Common condition 3",
+            test_method="wgs"
+        )
+
+        # Muliple panels and superpanels
         self.panel_1 = Panel.objects.create(
             external_id="20",
             panel_name="Test panel 1",
@@ -62,10 +80,63 @@ class TestGenerateGenepanels(TestCase):
         )
 
         self.superpanel_1 = SuperPanel.objects.create(
-            panel_name=,
-            panel_source=,
-            version=,
+            external_id="10",
+            panel_name="A superpanel",
+            panel_source="Test",
+            version="7",
             test_directory=True
         )
-        return None
+
+        # the panels and superpanels are each associated with a clinical
+        # indication
+        self.cip_1 = ClinicalIndicationPanel.objects.create(
+            clinical_indication=self.ci_1,
+            panel=self.panel_1
+        )
+
+        self.cip_2 = ClinicalIndicationPanel.objects.create(
+            clinical_indication=self.ci_2,
+            panel=self.panel_2
+        )
+
+        self.cisp_1 = ClinicalIndicationSuperPanel.objects.create(
+            clinical_indication=self.ci_3,
+            panel=self.superpanel_1
+        )
+
+        # Genes are linked to the panels and superpanels (they may overlap
+        # but not necessarily)
+        self.gene_1 = Gene.objects.create(
+            hgnc_id="HGNC:001",
+            gene_symbol="YFG1"
+        )
+        self.gene_2 = Gene.objects.create(
+            hgnc_id="HGNC:002",
+            gene_symbol="YFG2"
+        )
+        self.gene_3 = Gene.objects.create(
+            hgnc_id="HGNC:003",
+            gene_symbol="YFG3"
+        )
+
+        # finally, Ci-Panel or Ci-SuperPanel associations may
+        # exist in some TD releases, but not others
+        self.cip_td_old_1 = CiPanelTdRelease.objects.create(
+            ci_panel=self.cip_1, 
+            td_release=self.td_release_old,
+        )
+
+        self.cip_td_new_1 = CiPanelTdRelease.objects.create(
+            ci_panel=self.cip_1, 
+            td_release=self.td_release_new,
+        )
     
+        self.cip_td_old_2 = CiPanelTdRelease.objects.create(
+            ci_panel=self.cip_2, 
+            td_release=self.td_release_old,
+        )
+
+        self.cisp_td_new_1 = CiSuperpanelTdRelease.objects.create(
+            ci_superpanel=self.cisp_1,
+            td_release=self.td_release_new,
+        )
