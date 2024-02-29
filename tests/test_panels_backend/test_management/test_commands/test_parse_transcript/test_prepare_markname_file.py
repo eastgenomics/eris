2975725ecd5f4f60f9c6c936a1f267b015a34b09
corbin-chris <chris.corbin2@nhs.net>
2024-02-29 16:38:30 +0000

    Added ValueError test case for _prepare_markname_file

--- tests/test_panels_backend/test_management/test_commands/test_parse_transcript/test_prepare_markname_file.py
+++ tests/test_panels_backend/test_management/test_commands/test_parse_transcript/test_prepare_markname_file.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+import pandas as pd
+from unittest import mock
+
 from panels_backend.management.commands._parse_transcript import (
     _prepare_markname_file,
 )
@@ -20,3 +23,22 @@ class TestPrepareMarknameFile(TestCase):
         expected = {18222: [1], 7: [2]}
         result = _prepare_markname_file(self.sample_file_path)
         self.assertEqual(expected, result)
+
+    def test_expected_column_error(self):
+        """
+        CASE: The file is missing a required column, hgnc ID
+        EXPECT: A handy error is output to tell which cols are missing
+        """
+        with mock.patch("pandas.read_csv") as mock_df:
+            mock_return = pd.DataFrame(
+                {
+                    "gene_id": [1, 2],
+                }
+            )
+            mock_df.return_value = mock_return
+
+            expected_err = (
+                f"Missing columns in markname: \['hgncID'\]"
+            )
+            with self.assertRaisesRegex(ValueError, expected_err):
+                result = _prepare_markname_file("/dev/null")
