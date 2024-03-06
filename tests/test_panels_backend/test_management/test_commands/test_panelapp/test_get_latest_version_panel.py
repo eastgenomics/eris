8813729035b125eeffbc9c78845dbecc90aa5356
JasonLing95 <jason_ling95@hotmail.com>
2024-03-06 09:54:06 +0000

    IN-390 pr fix

--- tests/test_panels_backend/test_management/test_commands/test_panelapp/test_get_latest_version_panel.py
+++ tests/test_panels_backend/test_management/test_commands/test_panelapp/test_get_latest_version_panel.py
@@ -2,47 +2,26 @@ import json
 from unittest import mock
 from django.test import TestCase
 
-from panels_backend.management.commands.panelapp import (
-    get_latest_version_panel,
-    PanelClass,
-)
+from panels_backend.management.commands.panelapp import get_latest_version_panel
+
+from .mockresponse import MockResponse
 
 
 class TestGetLatestVersionPanel(TestCase):
-    def get_latest_version_panel(self):
+    @mock.patch('requests.get')
+    def test_get_latest_version_panel(self, mocked_panel):
         """
         Case: Fetch latest version given a panel id or specific version given a panel
-
-        NOTE: a bit of a weird test because these two functions
-        - get_latest_version_panel
-        - get_specific_version_panel
-        are essentially the same function that call another function
-        "get_panel_from_url" which is tested in test_get_panel_from_url.py
-
-        In order for the test to work, it is necessary to mock the output for two functions
-        nested within:
-        - get_panel_from_url
-        - _check_superpanel_status
 
         Expected behaviour: The function should return the latest version of a panel (mocked)
         """
 
-        with mock.patch(
-            "panels_backend.management.commands.panelapp.get_panel_from_url"
-        ) as mocked_panel:
-            with mock.patch(
-                "panels_backend.management.commands.panelapp._check_superpanel_status"
-            ) as mocked_superpanel_status:
-                mocked_superpanel_status.return_value = (
-                    False  # mocked panel is not a superpanel
-                )
+        mocked_panel.return_value = MockResponse(
+            json.load(open("testing_files/eris/panelapp_api_mocks/mock_panel.json")),
+            200,
+        )
 
-                mocked_panel.return_value = (
-                    PanelClass(**{"id": 3, "version": "4.1"}),
-                    False,
-                )  # mocked returned panel
+        panel, is_superpanel = get_latest_version_panel(3)
 
-                panel, is_superpanel = get_latest_version_panel(3)
-
-                assert not is_superpanel
-                assert panel.version == "4.1"
+        assert not is_superpanel
+        assert panel.version == "4.0"
